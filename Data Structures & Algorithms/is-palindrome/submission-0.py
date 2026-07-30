d76ab303834646977bfcc1f618c88ac6fc5e82d3
Nashita Dawood <nashud.06@gmail.com>
2026-07-30 19:11:50 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        new = ""
+        for ch in s.lower():
+            if ch.isalnum():
+                new += ch
+
+        return new == new[::-1]
