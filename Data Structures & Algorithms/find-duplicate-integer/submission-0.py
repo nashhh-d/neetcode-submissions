a24177863e53fe62087ab936607b6220c5395f1e
Nashita Dawood <nashud.06@gmail.com>
2026-08-23 19:33:22 +0530

    Add: find-duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-0.py
@@ -0,0 +1,22 @@
+class Solution:
+    def findDuplicate(self, nums):
+
+        slow = nums[0]
+        fast = nums[0]
+
+        
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+
+            if slow == fast:
+                break
+
+        # Find beginning of cycle
+        slow = nums[0]
+
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[fast]
+
+        return slow
